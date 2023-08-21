1258fe1ab772ed5c0faa544da5ed3326e4abe465
Soravee Rattanaapha <parker.soravee@gmail.com>
2023-08-21 23:22:41 +0700

    first commit

---
+++ code ทั้งหมด/lib/data/questions.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_application_1/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What is my favorite color?',
+    [
+      'Blue',
+      'Pink',
+      'Orange',
+      'Red',
+    ],
+  ),
+  QuizQuestion(
+    'Question 2',
+    [
+      'Choice2 1',
+      'Choice2 2',
+      'Choice2 3',
+      'Choice2 4',
+    ],
+  ),
+];
---
+++ code ทั้งหมด/lib/screens/questions_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/data/questions.dart';
+import 'package:flutter_application_1/widgets/answer_button.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({
+    super.key,
+    required this.onSelectAnswer,
+  });
+
+  final void Function(String answer) onSelectAnswer;
+
+  @override
+  State<QuestionsScreen> createState() => _QuestionsScreenState();
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  int currentQuestionIndex = 0;
+
+  void answerQustion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
+    setState(() {
+      // currentQuestionIndex = currentQuestionIndex + 1;
+      // currentQuestionIndex += 1;
+      currentQuestionIndex++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {
+                  answerQustion(answer);
+                },
+              );
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
